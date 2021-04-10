db591072244d66c4520b8aeacb10dd6c96047a27
RongY <45574293+RongYangAriel@users.noreply.github.com>
2021-04-10 16:30:35 -0400

    create a table to store all the matching score

--- src/components/MatchTable.js
+++ src/components/MatchTable.js
@@ -5,27 +5,71 @@ const standards = ['Location', 'TechStack', 'Seniority', 'IOM'];
 
 const MatchTable = (props) => {
 
-    const [matchData, useMatchData] = useState({});
+    const [matchData, setMatchData] = useState({});
 
-    const matchingAlgo = () => {
-        let standardColMap = {};
+    // Find the standarad col index in both excel
+    const buildStandardTable = () => {
+        let talentMap = {};
+        let jdMap = {};
         for (let standard of standards) {
             for (let index = 0; index < props.talent.rows[0].length; index++) {
                 if(props.talent.rows[0][index] === standard){
-                    standardColMap[standard] = index; 
+                    talentMap[standard] = index;
                 }
             }
         }
-        for (let candidate of props.talent.rows.slice(0)) {
-            console.log(candidate)
+        for (let standard of standards) {
+            for (let index = 0; index < props.jd.rows[0].length; index++) {
+                if(props.jd.rows[0][index] === standard){
+                    jdMap[standard] = index;
+
+                }
+            }
         }
 
+        return {talentMap, jdMap}
+    }
+
+    const {talentMap , jdMap} = buildStandardTable();
+
+    const matchingAlgo = ({talentMap, jdMap}) => {
+
+
+        // Key:talent Index; value: {jobIndex: score}
+        let matchResult = {};
+        
+        //
+        let talentTable = props.talent.rows.slice(0);
+        let jobTable = props.jd.rows.slice(0);
+
+
+        for (let talentIndex = 0; talentIndex < talentTable.length; talentIndex ++) {
+            
+            matchResult[talentIndex] = {};
+
+            for (let jobIndex = 0; jobIndex < jobTable.length; jobIndex ++ ) {
+                let jobTalentScore = 0;
+
+                for (let standard of standards) {
+                    // console.log(standard);
+
+                    if (talentTable[talentIndex][talentMap[standard]] == jobTable[jobIndex][jdMap[standard]]){
+                        jobTalentScore ++;
+                    }
+                }
+
+                matchResult[talentIndex][`${jobIndex}`] = jobTalentScore;
+            }
+        }
+        return matchResult
     };
 
-    matchingAlgo();
+
+
+    console.log(matchingAlgo({talentMap , jdMap}));
 
     return (
-        <OutTable data={props.talent.rows} columns={props.talent.cols}/>
+        <OutTable data={props.jd.rows} columns={props.jd.cols}/>
     )
 }
 
