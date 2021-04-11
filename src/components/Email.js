daeaadc671522b8b8da7a9e3aa4b20e8ae08ef55
RongY <45574293+RongYangAriel@users.noreply.github.com>
2021-04-11 15:38:16 -0400

    add subject and from input

--- src/components/Email.js
+++ src/components/Email.js
@@ -12,7 +12,7 @@ class Email extends Component {
                 body: "This is Kirby who has connected with you on LinkedIn several days ago. I would like to share the open opportunities that could be a good match for you.  Meanwhile, I’d love to send the job openings that could be a good fit along with our insights to you every week. Hope the information could help you more with your career choice.",
                 from: null,
                 to: null,
-
+                object: null
             },
         }
        
@@ -23,14 +23,34 @@ class Email extends Component {
     }
     
     handleChange = (event) => {
+        const newState = {...this.state.email, body: event.target.value};
         this.setState( {
-            email: {
-                body: event.target.value,
-            }
+            email: newState
         }, () => {
-            console.log(this.state.email.body);
+            console.log(this.state.email);
         });
     };
+
+    fromChangeHandler = (event) => {
+        const newState = {...this.state.email, from: event.target.value};
+        this.setState( {
+            email: newState
+        }, () => {
+            console.log(this.state.email);
+        });
+    };
+
+
+    subjectChangeHandler = (event) => {
+        const newState = {...this.state.email, subject: event.target.value};
+        this.setState( {
+            email: newState
+        }, () => {
+            console.log(this.state.email);
+        });
+    };
+
+
 
     handleSubmit = (event) => {
 
@@ -43,13 +63,17 @@ class Email extends Component {
           <div className="email">
             <div className="close" onClick={this.handleClick}>&times;    </div>
             <dl className="meta dl-horizontal">
-                <dt>From</dt>
+                <dt>From
+                    <input type="email" onChange={this.fromChangeHandler}></input>
+                </dt>
                 <dd>{this.props.from}</dd>
 
                 <dt>To</dt>
                 <dd>{this.props.to}</dd>
 
-                <dt>Subject</dt>
+                <dt>Subject
+                    <input type="text" onChange={this.subjectChangeHandler}></input>
+                </dt>
                 <dd>{this.props.subject}</dd>
             </dl>
             <form>
